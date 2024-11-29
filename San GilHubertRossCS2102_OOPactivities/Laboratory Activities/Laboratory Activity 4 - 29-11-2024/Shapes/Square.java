b56f4420d11d05c92b8f48841472dc9cf4aa7554
hubertsangil <163838170+hubertsangil@users.noreply.github.com>
2024-11-29 22:37:14 +0800

    Added Laboratory Activity 4.

---
+++ San GilHubertRossCS2102_OOPactivities/Laboratory Activities/Laboratory Activity 4 - 29-11-2024/Shapes/Square.java
@@ -0,0 +1,23 @@
+public class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public void printShapeType() {
+        System.out.println("Square");
+        
+    }
+}
